54185d4ad4bf547852f615c8f8f03b4d2c6c1235
shruthireddy-1505 <shruthireddyneelannolla@gmail.com>
2026-03-21 13:20:29 +0530

    Time: 26 ms (52.77%), Space: 24.6 MB (71.64%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,5 +1,6 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
+        """
         transpose = []
         count = 0
         n = len(grid)
@@ -18,4 +19,25 @@ class Solution:
                         l+=1
                         if l==n:
                             count+=1
+        return count
+        """
+        n = len(grid)
+        d={}
+        for i in grid:
+            tr = tuple(i)
+            if tr in d:
+                d[tr]+=1
+            else:
+                d[tr] = 1
+
+        count=0
+        for i in range(n):
+            col=[]
+            for j in range(n):
+                col.append(grid[j][i])
+
+            tc = tuple(col)      
+
+            if tc in d:
+                count+=d[tc]
         return count
